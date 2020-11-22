1b3800a46bc789100db2cbab8df9560cbf15cb13
Matthew Lemon <lemon@matthewlemon.com>
2020-11-22 19:53:22 +0000

    work

--- tw_hooks/scratch.pl
+++ tw_hooks/scratch.pl
@@ -29,8 +29,11 @@ if ($decoded_task->{scheduled} and (scalar grep {$_ eq "dft" } @{$tags})) {
     my $date = $scheduled_dt->day();
     my $month = $short_months[$scheduled_dt->month()-1];
     my $year = $scheduled_dt->year();
+    my $hr = $scheduled_dt->hour();
+    my $min = $scheduled_dt->minute();
+    my $time = $scheduled_dt->hms();
     # Convert it into Remind format
-    my $remind_line = "REM $date $month $year AT 10:00 $original_description";
+    my $remind_line = "REM $date $month $year AT $time $original_description";
     print $remind_line;
 } else {
    print encode_json $decoded_task; 
