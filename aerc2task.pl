0d3b4a22341fc6c81537c8c279de8cc5daa033cf
Matthew Lemon <y@yulqen.org>
2023-10-12 08:51:06 +0100

    Adds new aerc to task script

---
+++ aerc2task.pl
@@ -0,0 +1,17 @@
+#!/bin/env perl
+
+# this script is used to pipe an email from aerc using pipe -m ./aerc2task.pl to taskwarrior
+
+foreach my $line (<STDIN>) {
+    chomp;
+    if ($line =~ /^Subject/) {
+        print "Received: $line\n";
+        my @task_split = split(/Subject: /, $line);
+        $task = @task_split[1];
+        $task =~ s/TASK//g;
+        $task =~ s/WATCH//g;
+        print "So task is: $task\n";
+        `task add $task`
+    }
+}
+
