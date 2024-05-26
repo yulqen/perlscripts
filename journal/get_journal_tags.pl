bde34d4979749439079b3b615ee73bf2bec4bfc1
Matthew Lemon <y@yulqen.org>
2024-05-26 09:13:38 +0100

    Fixes old path to journal notes

--- journal/get_journal_tags.pl
+++ journal/get_journal_tags.pl
@@ -3,7 +3,7 @@ use warnings;
 
 my @tags;
 
-foreach my $fn (glob "~/Notes/journal/*.md") {
+foreach my $fn (glob "~/Documents/Notes/journal/home/*.md") {
     open my $FH, "<", $fn or die "Cannot open $fn";
     while (<$FH>) {
         while (/(\s:[A-Z]\w+)/g) {
@@ -12,6 +12,7 @@ foreach my $fn (glob "~/Notes/journal/*.md") {
                 
     }
 }
+# This just ensures the tags are unique
 my %truetags = map { $_, 1 } @tags;
 my @trutags = keys %truetags;
 print @trutags, "\n";
